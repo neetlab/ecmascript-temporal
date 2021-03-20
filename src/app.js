3b0945871ab929d6e1f315bb595f25c90dd410ed
Ryo Igarashi <n33t5hin@gmail.com>
2021-03-20 19:38:41 +0900

    Tried calendar

--- src/app.js
+++ src/app.js
@@ -3,11 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var proposal_temporal_1 = require("proposal-temporal");
 {
     var now = proposal_temporal_1.Temporal.now.instant();
+    // Note that a Temporal.Instant doesn't know about time zones.
+    // For the exact time in a specific time zone,
     console.log(now.toLocaleString('ja-JP'));
 }
 {
+    var now = proposal_temporal_1.Temporal.now.plainDate('2020-01-01');
+    console.log(now.withCalendar('gregory').add({ years: 1 }));
+}
+{
+    // ZonedDateTimeを作らないと { years } とかが入ったaddができない？
     var now = proposal_temporal_1.Temporal.now.zonedDateTime(proposal_temporal_1.Temporal.Calendar.from('iso8601'));
     var d = proposal_temporal_1.Temporal.Duration.from({ years: 1 });
     var twoYearsAndADayLater = now.add(d);
     console.log(twoYearsAndADayLater.toString());
+}
+{
+    // 暦
+    var gregory = proposal_temporal_1.Temporal.now.zonedDateTime(proposal_temporal_1.Temporal.Calendar.from('gregory'));
+    console.log(gregory.toString());
 }
--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,28 @@ import { Temporal } from "proposal-temporal";
 
 {
   const now = Temporal.now.instant();
+  // Note that a Temporal.Instant doesn't know about time zones.
+  // For the exact time in a specific time zone,
   console.log(now.toLocaleString('ja-JP'));
+}
+
+{
+  const now = Temporal.now.plainDate('2020-01-01')
+  // カレンダー指定
+  console.log(now.withCalendar('gregory').add({ years: 1 }).toString());
 }
 
 
 {
+  // ZonedDateTimeを作らないと { years } とかが入ったaddができない？
   const now = Temporal.now.zonedDateTime(Temporal.Calendar.from('iso8601'))
   const d = Temporal.Duration.from({ years: 1 });
   const twoYearsAndADayLater = now.add(d);
   console.log(twoYearsAndADayLater.toString());
+}
+
+{
+  // 暦
+  const gregory = Temporal.now.zonedDateTime(Temporal.Calendar.from('gregory'))
+  console.log(gregory.toString());
 }
